e995ce3bb9554dfef4a24aaa12fb8df61d2698fd
Atsushi.Taketsuji <at.0130.145@gmail.com>
2021-05-22 20:37:32 +0900

    Initial Commit

---
+++ practice7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice7
+//
+//  Created by 竹辻篤志 on 2021/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
